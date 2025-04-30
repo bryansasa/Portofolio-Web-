415f99b106b07186c3724026b193fa01553db3e7
tinnreinme <bryansasabone94@gmail.com>
2025-04-30 22:19:12 +0900

    ubah node js

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
           </div>
           <div className="flex-1 flex justify-center">
             <Avatar className="h-64 w-64">
-              <AvatarImage src="/cooked.jpg?height=256&width=256" alt="Foto Profil" />
+              <AvatarImage src="/cooked.jpg?height=512&width=512" alt="Foto Profil" />
               <AvatarFallback>NS</AvatarFallback>
             </Avatar>
           </div>
